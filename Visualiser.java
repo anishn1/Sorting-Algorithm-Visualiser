bec795f84e20825661053f58abc498cd6bfe6c21
Anish <ab3924.ic.ac.uk>
2025-09-06 14:20:41 +0100

    Added bubble sort and a way to count the number of comparisons

--- Visualiser.java
+++ Visualiser.java
@@ -2,7 +2,17 @@ public class Visualiser {
     // private static final int SIZE = 10; // sizeof array
     // private static final int WIDTH = 50; // max num of # for bar
 
+    public static void main(String[] args) throws InterruptedException {
+        int[] arr = {3,9,4,1,7,8,2,5,6};
+        bubbleSort(arr);
+        System.out.printf("%n%n%n num of comparisons = %d%n", num);
+    }
+
+    public static int num = 0;
+
     private static void printArray(int[] arr, int elem1, int elem2) {
+        num++;
+        System.out.println("-----------------------------");
         int max = 0;
         for (int i : arr) {
             if (i > max) {
@@ -17,6 +27,25 @@ public class Visualiser {
                 System.out.printf("%d:  %s%n", i, bar);
             }
         }
+        System.out.println("-----------------------------");
     }
 
+    private static void bubbleSort(int[] arr) throws InterruptedException {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+                printArray(arr, j, j+1);
+                Thread.sleep(300);
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
